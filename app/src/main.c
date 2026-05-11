fefbba66b975a0caeaad7c7295074ac89c74d01f
Francois Gervais <francoisgervais@gmail.com>
2026-05-11 18:20:32 -0400

    Can connect to wifi
    Connect at 5 GHz, 20 MHz.

--- app/src/main.c
+++ app/src/main.c
@@ -2,6 +2,7 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/watchdog.h>
 #include <zephyr/kernel.h>
+#include <zephyr/net/wifi_mgmt.h>
 #include <zephyr/pm/device.h>
 #include <zephyr/debug/thread_analyzer.h>
 
@@ -19,9 +20,64 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
 #define BUTTON_PRESS_EVENT		BIT(0)
 
+#define MACSTR "%02X:%02X:%02X:%02X:%02X:%02X"
+#define NET_EVENT_WIFI_MASK                                                                        \
+	(NET_EVENT_WIFI_CONNECT_RESULT | NET_EVENT_WIFI_DISCONNECT_RESULT |                        \
+	 NET_EVENT_WIFI_AP_ENABLE_RESULT | NET_EVENT_WIFI_AP_DISABLE_RESULT |                      \
+	 NET_EVENT_WIFI_AP_STA_CONNECTED | NET_EVENT_WIFI_AP_STA_DISCONNECTED)
+
+
 
 static K_EVENT_DEFINE(button_events);
 
+static struct net_if *sta_iface;
+static struct wifi_connect_req_params sta_config;
+static struct net_mgmt_event_callback cb;
+
+
+static void wifi_event_handler(struct net_mgmt_event_callback *cb, uint64_t mgmt_event,
+			       struct net_if *iface)
+{
+	switch (mgmt_event) {
+	case NET_EVENT_WIFI_CONNECT_RESULT: {
+		LOG_INF("Connected to %s", CONFIG_APP_WIFI_SSID);
+		break;
+	}
+	case NET_EVENT_WIFI_DISCONNECT_RESULT: {
+		LOG_INF("Disconnected from %s", CONFIG_APP_WIFI_SSID);
+		break;
+	}
+	default:
+		break;
+	}
+}
+
+
+static int connect_to_wifi(void)
+{
+	if (!sta_iface) {
+		LOG_INF("STA: interface no initialized");
+		return -EIO;
+	}
+
+	sta_config.ssid = (const uint8_t *)CONFIG_APP_WIFI_SSID;
+	sta_config.ssid_length = sizeof(CONFIG_APP_WIFI_SSID) - 1;
+	sta_config.psk = (const uint8_t *)CONFIG_APP_WIFI_PSK;
+	sta_config.psk_length = sizeof(CONFIG_APP_WIFI_PSK) - 1;
+	sta_config.security = WIFI_SECURITY_TYPE_PSK;
+	sta_config.channel = WIFI_CHANNEL_ANY;
+	sta_config.band = WIFI_FREQ_BAND_2_4_GHZ;
+
+	LOG_INF("Connecting to SSID: %s\n", sta_config.ssid);
+
+	int ret = net_mgmt(NET_REQUEST_WIFI_CONNECT, sta_iface, &sta_config,
+			   sizeof(struct wifi_connect_req_params));
+	if (ret) {
+		LOG_ERR("Unable to Connect to (%s)", CONFIG_APP_WIFI_SSID);
+	}
+
+	return ret;
+}
 
 int main(void)
 {
@@ -55,6 +111,23 @@ int main(void)
 	} else {
 		module_set_state(MODULE_STATE_READY);
 	}
+
+
+
+
+	k_sleep(K_SECONDS(5));
+
+	// net_mgmt_init_event_callback(&cb, wifi_event_handler, NET_EVENT_WIFI_MASK);
+	// net_mgmt_add_event_callback(&cb);
+
+	// sta_iface = net_if_get_wifi_sta();
+
+	// connect_to_wifi();
+
+
+
+
+
 
 	LOG_INF("🆗 initialized");
 
